72f0deee283a64096e53dc01cc163fecc5b54059
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-07-27 14:54:00 -0300

    basic script with Playwright using functions & selectors

---
+++ playwright.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testMatch: ["Login.test.ts"],
+});
