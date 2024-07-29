b62c26143d8ef5d69908878fb7e6a21ddab852a0
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-07-28 21:06:30 -0300

    Playwright’s futuristic features

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,4 +12,25 @@ import { defineConfig } from "@playwright/test";
  */
 export default defineConfig({
   testMatch: ["Login.test.ts"],
+  use: {
+    headless: false,
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+  },
+  retries: 0,
+  reporter: [
+    ["dot"],
+    [
+      "json",
+      {
+        outputFile: "jsonReports/jsonReport.json",
+      },
+    ],
+    [
+      "html",
+      {
+        open: "never",
+      },
+    ],
+  ],
 });
--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,9 +1,7 @@
 import test, { chromium } from "@playwright/test";
 
 test("Login test demo", async () => {
-  const browser = await chromium.launch({
-    headless: true,
-  });
+  const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://ecommerce-playground.lambdatest.io/");
